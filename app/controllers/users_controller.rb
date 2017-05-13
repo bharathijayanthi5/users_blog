87476045766c81ddd2d8bb6978e3231c7763b01b
bharathijayanthi5 <bharathi@gurujada.org>
2017-05-13 11:24:29 +0530

    added users and articles model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def index
+    @users = User.all
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "welcome #{@user.username} you have signed up successfully"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+@user = User.update(user_params)
+if @user.save
+  flash[:notice] = "your profile was successfully updated"
+  redirect to @user
+else
+  render 'edit'
+end
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
