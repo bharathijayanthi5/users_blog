8b5bcca3716992aff00717054f2d2925d0b8dc47
bharathijayanthi5 <bharathi@gurujada.org>
2017-05-16 11:43:59 +0530

    added ck editor to create the new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
+    
   end
   def new
     @article = Article.new
@@ -16,13 +18,13 @@ class ArticlesController < ApplicationController
     end
   end
   def show
-    @article = Article.find(params[:id])
+
   end
   def edit
-      @article = Article.find(params[:id])
+
     end
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
     flash[:notice] = "Article was successfully updated"
     redirect_to @article
@@ -31,12 +33,15 @@ class ArticlesController < ApplicationController
   end
   end
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to articles_path
   end
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :articles
 get 'signup', to: "users#new"
-resources :users, except: [:new]  
+resources :users, except: [:new]
+get 'login', to: "sessions#new"
+post 'login', to: "sessions#create"
+delete 'logout', to: "sessions#destroy"  
 end
